11e8063cc0645bf91f810bc7e84b096ad6ea4c39
NokaK <nona.kipiani07b@geolab.edu.ge>
2018-04-14 00:33:11 +0400

    change slider

--- js/script.js
+++ js/script.js
@@ -1,26 +1,7 @@
-$('.center').slick({
-  centerMode: true,
-  centerPadding: '60x',
-  slidesToShow: 3,
-  arrows: true,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: true,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 3
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
-    }
-  ]
+
+$(function() {
+  fr = new FilmRoll({
+    container: '#film_roll',
+    height: 800
+  });
 });
